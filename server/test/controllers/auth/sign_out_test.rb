4143090c55b0e75dee487d022bac2828c6e92e97
JP Rosevear <jp@codalio.com>
2024-08-06 23:28:21 -0400

    task: do setup instead of prepare for controller test cases

--- server/test/controllers/auth/sign_out_test.rb
+++ server/test/controllers/auth/sign_out_test.rb
@@ -3,63 +3,55 @@
 require "test_helper"
 
 class SignOutSuccessTest < Rhino::TestCase::ControllerTest
+  def setup
+    sign_in
+  end
+
   test "returns 200 when successful" do
-    prepare
+    sign_out
     assert_response_ok
   end
 
   test "deletes token when successful" do
-    prepare
-    assert_equal 0, @current_user.reload.tokens.size
+    assert_changes -> { @current_user.reload.tokens.size }, from: 1, to: 0 do
+      sign_out
+    end
   end
 
   test "clears cookie when successful" do
-    prepare
+    sign_out
     assert_deleted_cookie DeviseTokenAuth.cookie_name
   end
-
-  private
-    def prepare
-      sign_in
-      assert_equal 1, @current_user.reload.tokens.size
-      sign_out
-    end
 end
 
 class SignOutNonExistingTokenTest < Rhino::TestCase::ControllerTest
+  def setup
+    sign_in
+    delete_user_tokens
+    sign_out
+  end
+
   test "returns 404 when token doesn't exist" do
-    prepare
     assert_response_not_found
   end
 
   test "clears cookie when token doesn't exist" do
-    prepare
     assert_deleted_cookie DeviseTokenAuth.cookie_name
   end
-
-  private
-    def prepare
-      sign_in
-      delete_user_tokens
-      sign_out
-    end
 end
 
 class SignOutAbsentCookieTest < Rhino::TestCase::ControllerTest
+  def setup
+    sign_in
+    # doesn't send the auth cookie in the request
+    sign_out headers: empty_auth_cookie_header
+  end
+
   test "returns 404 when cookie is not sent" do
-    prepare
     assert_response_not_found
   end
 
   test "clears cookie when cookie is not sent" do
-    prepare
     assert_deleted_cookie DeviseTokenAuth.cookie_name
   end
-
-  private
-    def prepare
-      sign_in
-      # doesn't send the auth cookie in the request
-      sign_out headers: empty_auth_cookie_header
-    end
 end
