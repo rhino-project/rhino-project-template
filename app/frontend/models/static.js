408c26f879b12669e8e844c6bb249018d4f13b85
JP Rosevear <jp@codalio.com>
2024-09-09 17:03:05 -0400

    chore: vite server basics are now handled by vite_ruby

--- app/frontend/models/static.js
+++ app/frontend/models/static.js
@@ -940,7 +940,7 @@ const api = {
     "x-rhino": {
       "modules": {
         "rhino": {
-          "version": "0.21.0.beta.10",
+          "version": "0.21.0.beta.17",
           "authOwner": "user",
           "baseOwner": "user",
           "oauth": [
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import eslint from 'vite-plugin-eslint';
 import { RhinoProjectVite } from '@rhino-project/vite-plugin-rhino';
@@ -7,17 +7,7 @@ import RubyPlugin from 'vite-plugin-ruby';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // https://main.vitejs.dev/config/#using-environment-variables-in-config
-  const env = loadEnv(mode, process.cwd(), '');
-
   return {
-    // Allow .env to specify port
-    server: {
-      // Allow serving from 0.0.0.0, 127.0.0.1 and localhost
-      host: true,
-      port: Number(env.PORT) || 3001
-    },
-
     plugins: [
       RubyPlugin(),
       RhinoProjectVite({ enableJsxInJs: false }),
