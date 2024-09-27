048bea650b272178e89328ef93a9c3c6a4f11680
JP Rosevear <jp@codalio.com>
2024-09-27 14:14:11 -0400

    chore: Remove support for FRONT_END_LOCAL_URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-ORIGIN_ENV = [ENV["FRONT_END_URL"], ENV["FRONT_END_LOCAL_URL"]].compact.freeze
+ORIGIN_ENV = [ENV["FRONT_END_URL"]].compact.freeze
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
