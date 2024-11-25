7877a877dcd390ee624d22a2f89572db00d0f7de
JP Rosevear <jp@codalio.com>
2024-11-25 08:02:52 -0500

    chore: make indent better and automatic for IDE usage

--- db/migrate/20210302125946_fix_create_stripe_customer.rb
+++ db/migrate/20210302125946_fix_create_stripe_customer.rb
@@ -2,6 +2,6 @@ require_relative "20210219193346_create_stripe_customers"
 
 class FixCreateStripeCustomer < ActiveRecord::Migration[6.0]
   def change
-      revert CreateStripeCustomers
+    revert CreateStripeCustomers
   end
 end
