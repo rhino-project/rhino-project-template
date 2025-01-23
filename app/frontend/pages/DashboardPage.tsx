5956cf58445604668d274f17da837049ae0dc245
JP Rosevear <jp@codalio.com>
2025-01-22 19:18:05 -0500

    alpha: iconfiy

--- app/frontend/pages/DashboardPage.tsx
+++ app/frontend/pages/DashboardPage.tsx
@@ -165,7 +165,7 @@ const GetStarted = () => {
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <Icon
                   className="w-8 h-8 mb-2 text-foreground-400"
-                  icon="upload"
+                  icon="bi:upload"
                 />
                 <p className="mb-2 text-sm text-foreground-500">
                   <span className="font-semibold">Click to upload</span> or drag
@@ -240,7 +240,7 @@ const GetStarted = () => {
       <Button
         color="primary"
         variant="shadow"
-        startContent={<Icon icon="house" />}
+        startContent={<Icon icon="bi:house" />}
       >
         Learn NextUI
       </Button>
