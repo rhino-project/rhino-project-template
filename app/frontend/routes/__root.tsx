85e3fe9047e2817b367592ccc21f6a9c90985b0a
JP Rosevear <jp@codalio.com>
2025-02-12 18:45:09 -0500

    feat: update

--- app/frontend/routes/__root.tsx
+++ app/frontend/routes/__root.tsx
@@ -1,20 +1,31 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { HeroUIProvider, ToastProvider } from '@rhino-project/ui-heroui';
+import { PageAnalytics } from '@rhino-project/core/components/analytics';
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  )
+  component: () => {
+    return (
+      <HeroUIProvider
+        // navigate={navigate}
+        // useHref={useHref}
+        labelPlacement="inside"
+      >
+        <ToastProvider />
+        {/* <PageAnalytics> */}
+        <div className="p-2 flex gap-2">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{' '}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <hr />
+        <Outlet />
+        {/* </PageAnalytics> */}
+        <TanStackRouterDevtools />
+      </HeroUIProvider>
+    );
+  }
 });
