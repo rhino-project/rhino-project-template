0cafd3754c4f5d5ac168a87ae1663b949db3267b
JP Rosevear <jp@codalio.com>
2025-02-12 18:16:24 -0500

    feat: update

--- app/frontend/Root.tsx
+++ app/frontend/Root.tsx
@@ -52,6 +52,23 @@ import { PrimaryNavigation } from './components/app/PrimaryNavigation';
 import { SecondaryNavigation } from './components/app/SecondaryNavigation';
 import DashboardPage from './pages/DashboardPage';
 
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+
 const LazyDesignSystemRoute = lazy(
   () => import('./pages/design/DesignSystemPage')
 );
