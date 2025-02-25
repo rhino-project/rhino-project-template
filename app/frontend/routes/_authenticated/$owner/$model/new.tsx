57ec700882ff63e74bb55778ff9cf466c9c9d309
JP Rosevear <jp@codalio.com>
2025-02-25 12:09:01 -0500

    feat: more typing tests

--- app/frontend/routes/_authenticated/$owner/$model/new.tsx
+++ app/frontend/routes/_authenticated/$owner/$model/new.tsx
@@ -1,12 +1,26 @@
-import { ModelCreatePage } from '@rhino-project/ui-heroui';
+import { RhinoResourceName } from '@rhino-project/core';
+import { ModelCreate, ModelCreatePage } from '@rhino-project/ui-heroui';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/$owner/$model/new')({
+  params: {
+    parse: (rawParams) => {
+      const model = rawParams.model as RhinoResourceName;
+
+      return { model };
+    }
+  },
+  validateSearch: (search: Record<string, unknown>): { parentId: number } => {
+    return {
+      parentId: Number(search?.parentId)
+    };
+  },
   component: RouteComponent
 });
 
 function RouteComponent() {
   const { model } = Route.useParams();
+  const { parentId } = Route.useSearch();
 
-  return <ModelCreatePage model={model} />;
+  return <ModelCreate model={model} parentId={parentId} />;
 }
