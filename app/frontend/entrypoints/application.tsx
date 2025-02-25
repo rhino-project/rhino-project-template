f8d0745c48f62bc99daef4c174fca1cdb27b010d
JP Rosevear <jp@codalio.com>
2025-02-25 08:33:54 -0500

    feat: context only resources

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import modelLoader from '@rhino-project/core/models';
-
 import '@/styles/global.css';
 import { components } from '../models/models';
+import Root from '../Root';
 
 declare module '@rhino-project/core' {
   type SchemaToResource = {
@@ -14,24 +13,10 @@ declare module '@rhino-project/core' {
   interface Resources extends SchemaToResource {}
 }
 
-// declare module '@react-types/shared' {
-//   interface RouterConfig {
-//     routerOptions: NavigateOptions;
-//   }
-// }
-
-modelLoader.loadModels().then(async () => {
-  // Import the Root dynamically so that other modelLoader uses are assured
-  // to have access to the already loaded models
-
-  const { default: Root } = await import('@/Root');
-
-  const container = document.getElementById('root');
-  // @ts-expect-error createRoot is not yet in the types
-  const root = createRoot(container);
-  root.render(
-    <StrictMode>
-      <Root />
-    </StrictMode>
-  );
-});
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <Root />
+  </StrictMode>
+);
