7c6d7dcfb86bb685b9d48a9839fe744c0f107672
JP Rosevear <jp@codalio.com>
2025-02-25 14:55:14 -0500

    feat: index

--- app/frontend/routes/_authenticated/$owner/$model/index.tsx
+++ app/frontend/routes/_authenticated/$owner/$model/index.tsx
@@ -1,5 +1,5 @@
 import { RhinoResourceName } from '@rhino-project/core';
-import { ModelIndex, ModelIndexPage } from '@rhino-project/ui-heroui';
+import { ModelIndex } from '@rhino-project/ui-heroui';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/$owner/$model/')({
