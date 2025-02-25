1b912340bfcb7bf032fe5088ba7c666d230f5f28
JP Rosevear <jp@codalio.com>
2025-02-25 11:25:27 -0500

    feat: better typing

--- app/frontend/routes/_authenticated/$owner/$model/$id.index.tsx
+++ app/frontend/routes/_authenticated/$owner/$model/$id.index.tsx
@@ -1,12 +1,21 @@
-import { ModelShowPage } from '@rhino-project/ui-heroui';
+import { RhinoResourceName } from '@rhino-project/core';
+import { ModelShow } from '@rhino-project/ui-heroui';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/$owner/$model/$id/')({
+  params: {
+    parse: (rawParams) => {
+      const model = rawParams.model as RhinoResourceName;
+      const id = rawParams.id as string;
+
+      return { model, id };
+    }
+  },
   component: RouteComponent
 });
 
 function RouteComponent() {
-  const { model } = Route.useParams();
+  const { model, id } = Route.useParams();
 
-  return <ModelShowPage model={model} />;
+  return <ModelShow model={model} modelId={id} />;
 }
