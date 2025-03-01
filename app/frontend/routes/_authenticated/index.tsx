c8712a975564b55e52aaf7ba359b8e1346707212
JP Rosevear <jp@codalio.com>
2025-02-28 23:17:49 -0500

    alpha: tidy

--- app/frontend/routes/_authenticated/index.tsx
+++ app/frontend/routes/_authenticated/index.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/')({
   beforeLoad: ({ context: { rhino } }) => {
-    console.log('beforeLoad _authenticated', rhino.baseOwner);
     // FIXME: Support remember most recent with local storage
     throw redirect({
       to: `/$owner`,
