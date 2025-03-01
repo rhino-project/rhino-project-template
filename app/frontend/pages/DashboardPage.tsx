3d9b0d215e3ab2081a95981b8cda5cd5627a3cb9
JP Rosevear <jp@codalio.com>
2025-02-28 22:45:48 -0500

    alpha: fix

--- app/frontend/pages/DashboardPage.tsx
+++ app/frontend/pages/DashboardPage.tsx
@@ -4,7 +4,11 @@ import { useUser } from '@rhino-project/core/hooks';
 const DashboardPage = () => {
   const user = useUser();
 
-  return <BaseAuthedPage>Welcome ${user?.name || user?.email}</BaseAuthedPage>;
+  return (
+    <BaseAuthedPage>
+      <h4>Welcome {user?.name || user?.email}</h4>
+    </BaseAuthedPage>
+  );
 };
 
 export default DashboardPage;
