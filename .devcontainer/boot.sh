550a9cc7e13d0a6161a4a74a0371eeb1fc137fe8
JP Rosevear <jp@codalio.com>
2025-03-06 11:21:46 -0500

    fix: no need to copy db

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -7,9 +7,6 @@ echo "Installing dependencies..."
 bundle install
 yarn install
 
-echo "Copying database.yml..."
-cp -n config/database.yml.example config/database.yml
-
 echo "Creating database..."
 bin/rails db:prepare 
 
