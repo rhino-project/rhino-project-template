5313e75ddbab9f4087a147d11067b18dea3d1916
JP Rosevear <jp@codalio.com>
2025-03-18 13:10:17 -0400

    alpha: de-dupe dates

--- app/frontend/pages/DashboardPage.tsx
+++ app/frontend/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import { BaseAuthedPage } from '@rhino-project/ui-heroui';
+import { BaseAuthedPage, Link } from '@rhino-project/ui-heroui';
 import { useUser } from '@rhino-project/core/hooks';
 
 const DashboardPage = () => {
@@ -7,6 +7,7 @@ const DashboardPage = () => {
   return (
     <BaseAuthedPage>
       <h4>Welcome {user?.name || user?.email}</h4>
+      <Link href="/logout">Logout</Link>
     </BaseAuthedPage>
   );
 };
